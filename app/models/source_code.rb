5dbac44c9c691292cee57d0870a9ed749b8fd65c
Kouji Takao <kouji.takao@gmail.com>
2013-12-16 22:45:35 +0900

    SourceCode#check_syntaxを追加

--- app/models/source_code.rb
+++ app/models/source_code.rb
@@ -1,5 +1,23 @@
 # -*- coding: utf-8 -*-
 
+require 'tempfile'
+require 'open3'
+
 # ソースコードを表現するモデル
 class SourceCode < ActiveRecord::Base
+  # シンタックスをチェックする
+  def check_syntax
+    tempfile = Tempfile.new('smalruby-editor')
+    tempfile.write(self.data)
+    path = tempfile.path
+    tempfile.close
+    stdout_str, stderr_str, status = *Open3.capture3("ruby -c #{path}")
+    if status.success?
+      return []
+    end
+    stderr_str.lines.map { |line|
+      md = /^.*:(\d+): (.*)$/.match(line)
+      { row: md[1].to_i, column: 0, message: md[2] }
+    }
+  end
 end
--- spec/models/source_code_spec.rb
+++ spec/models/source_code_spec.rb
@@ -1,5 +1,33 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
-describe SourceCode do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe SourceCode, 'Rubyのソースコードを表現するモデル' do
+  describe '#check_syntax', 'シンタックスをチェックする' do
+    subject { source_code.check_syntax }
+
+    context 'シンタックスが正しい場合' do
+      let(:source_code) {
+        SourceCode.new(data: <<-EOS.strip_heredoc)
+          puts "Hello, World!"
+        EOS
+      }
+
+      it { should be_empty }
+    end
+
+    context 'シンタックスが正しくない場合' do
+      let(:source_code) {
+        SourceCode.new(data: <<-EOS.strip_heredoc)
+          puts Hello, World!"
+        EOS
+      }
+
+      it { should_not be_empty }
+      it {
+        should include(row: 1, column: 0,
+                       message: "syntax error, unexpected tSTRING_BEG, expecting keyword_do or '{' or '('")
+      }
+      it {should include(row: 1, column: 0, message: 'unterminated string meets end of file') }
+    end
+  end
 end
