a5bd722fc76bfbe2c250c512c2ea5bbc90234c08
Kouji Takao <kouji.takao@gmail.com>
2014-03-05 02:20:38 +0900

    motion_turnもレシーバを明記できるように修正

--- app/models/concerns/ruby_to_block/block/motion_turn.rb
+++ app/models/concerns/ruby_to_block/block/motion_turn.rb
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 module RubyToBlock
   module Block
-    class MotionTurn < Base
-      blocknize '^\s*turn\s*$', statement: true
+    class MotionTurn < CharacterMethodCall
+      blocknize '^\s*' + CHAR_RE + 'turn\s*$', statement: true
 
       def self.process_match_data(md, context)
-        return false unless context.receiver
+        md2 = regexp.match(md[type])
 
-        context.add_block(new)
+        block = new
+        _, context.current_block =
+          *add_child_or_create_character_new_block(context, md2[1], block)
 
         true
       end
--- spec/models/concerns/ruby_to_block/block/motion_turn_spec.rb
+++ spec/models/concerns/ruby_to_block/block/motion_turn_spec.rb
@@ -5,52 +5,49 @@ require_relative 'shared/block_examples'
 # rubocop:disable EmptyLines, LineLength
 
 describe RubyToBlock::Block::MotionTurn, to_blocks: true do
-  _data = <<-EOS.strip_heredoc
-require "smalruby"
-
-car1 = Character.new(costume: "car1.png", x: 0, y: 0, angle: 0)
-
-car1.on(:start) do
-  turn
-end
+  _parts = <<-EOS
+turn
   EOS
-  describe compact_source_code(_data) do
-    __data = _data
-    let(:data) { __data }
+  describe compact_source_code(_parts), on_start_data: true do
+    __parts = _parts
+    let(:parts) { __parts }
 
     it '結果が正しいこと' do
-      should eq(<<-XML.strip_heredoc)
-<xml xmlns="http://www.w3.org/1999/xhtml">
-  <character name="car1" x="0" y="0" angle="0" costumes="car1.png" />
-  <block type="character_new">
-    <field name="NAME">car1</field>
-    <statement name="DO">
-      <block type="events_on_start">
-        <statement name="DO">
+      should eq_block_xml(<<-XML)
           <block type="motion_turn" />
-        </statement>
-      </block>
-    </statement>
-  </block>
-</xml>
       XML
     end
   end
 
-  _data = <<-EOS.strip_heredoc
-turn
+  _parts = <<-EOS
+car1.turn
   EOS
-  describe compact_source_code(_data) do
-    __data = _data
-    let(:data) { __data }
+  describe compact_source_code(_parts), character_new_data: true do
+    __parts = _parts
+    let(:parts) { __parts }
 
     it '結果が正しいこと' do
-      should eq(<<-XML.strip_heredoc)
-<xml xmlns="http://www.w3.org/1999/xhtml">
+      should eq_block_xml(<<-XML)
+    <field name="NAME">car1</field>
+    <statement name="DO">
+      <block type="motion_turn" />
+    </statement>
+      XML
+    end
+  end
+
+  _parts = <<-EOS
+turn
+  EOS
+  describe compact_source_code(_parts) do
+    __parts = _parts
+    let(:data) { __parts }
+
+    it '結果が正しいこと' do
+      should eq_block_xml(<<-XML)
   <block type="ruby_statement">
     <field name="STATEMENT">turn</field>
   </block>
-</xml>
       XML
     end
   end
