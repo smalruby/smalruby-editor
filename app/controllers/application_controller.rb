f7807a24ae0060e59667724829e7200e6b9c394f
Kouji Takao <kouji.takao@gmail.com>
2015-05-20 22:31:50 +0900

    [fix #94530458] store locale in session.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,8 +24,9 @@ class ApplicationController < ActionController::Base
 
   # ロケールの設定
   def set_locale
-    I18n.locale = params[:locale] ||
+    I18n.locale = params[:locale] || session[:locale] ||
       extract_locale_from_accept_language_header || I18n.default_locale
+    session[:locale] = I18n.locale
     Rails.application.routes.default_url_options[:locale] = I18n.locale
   end
 
