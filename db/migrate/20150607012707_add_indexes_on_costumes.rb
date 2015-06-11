6605c19755a54317545eef13f25b20e81d791903
Kouji Takao <kouji.takao@gmail.com>
2015-06-11 14:31:46 +0900

    Revert "suppress warning."
    This reverts commit 531cf330d25738915a2db625d2880a77dbab90ad.

--- db/migrate/20150607012707_add_indexes_on_costumes.rb
+++ db/migrate/20150607012707_add_indexes_on_costumes.rb
@@ -1,13 +1,13 @@
 class AddIndexesOnCostumes < ActiveRecord::Migration
   def up
-    add_index "costumes", :name
-    add_index "costumes", :position
-    add_index "costumes", :user_id
+    add_index :costumes, :name
+    add_index :costumes, :position
+    add_index :costumes, :user_id
   end
 
   def down
-    remove_index "costumes", :name
-    remove_index "costumes", :position
-    remove_index "costumes", :user_id
+    remove_index :costumes, :name
+    remove_index :costumes, :position
+    remove_index :costumes, :user_id
   end
 end
